e2f025e1efc51870a20efd2a1831d2e240c1fc90
Gwilides <83014200+Gwilides@users.noreply.github.com>
2025-03-14 19:34:17 +0200

    Task 2 is completed

---
+++ Lab3/2.php
@@ -0,0 +1,11 @@
+<?php
+$a = 8;
+$b = 8.8;
+$lastMonth = 10000;
+$thisMonth = 7000;
+
+echo $a;
+echo "\n";
+echo $b, "\n";
+echo $a + 4, "\n";
+echo "The difference between expenses: ", $lastMonth - $thisMonth;
