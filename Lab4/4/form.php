b11c29ed6d47b3d241b0ec0bcd3db72b005d8260
Gwilides <83014200+Gwilides@users.noreply.github.com>
2025-04-05 23:10:29 +0200

    Added form.php

---
+++ Lab4/4/form.php
@@ -0,0 +1,38 @@
+<?php
+session_start();
+
+$name = $_POST['name'];
+$type = $_POST['type'];
+$age = $_POST['age'];
+
+$_SESSION['user'] = [
+    'name' => $name,
+    'type' => $type,
+    'age' => $age
+];
+header('Location profile.php');
+exit();
+?>
+
+<!DOCTYPE html>
+<html lang="ru">
+    <head>
+        <meta charset="UTF-8">
+        <title>Taks 4</title>
+    </head>
+    <body>
+        <h1>Введите данные питомца</h1>
+        <form method="POST">
+            <label for="name">Имя</label>
+            <input type="text" id="name" name="name" required><br><br>
+            
+            <label for="type">Вид</label>
+            <input type="text" id="type" name="type" required><br><br>
+            
+            <label for="age">Возраст</label>
+            <input type="text" id="age" name="age" required><br><br>
+            
+            <button>Отправить</button>
+        </form>
+    </body>
+</html>
