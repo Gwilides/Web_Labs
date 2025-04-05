6240ff22d0feed27ccbac8a005a056d749ac2fa0
Gwilides <83014200+Gwilides@users.noreply.github.com>
2025-04-05 23:42:50 +0200

    Fixed index.php

--- Lab4/4/index.php
+++ Lab4/4/index.php
@@ -1,24 +1,25 @@
 <?php
 session_start();
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $name = $_POST['name'];
+    $type = $_POST['type'];
+    $age = $_POST['age'];
 
-$name = $_POST['name'];
-$type = $_POST['type'];
-$age = $_POST['age'];
-
-$_SESSION['user'] = [
-    'name' => $name,
-    'type' => $type,
-    'age' => $age
-];
-header('Location profile.php');
-exit();
+    $_SESSION['user'] = [
+        'name' => $name,
+        'type' => $type,
+        'age' => $age
+    ];
+    header('Location: profile.php');
+    exit();
+}
 ?>
 
 <!DOCTYPE html>
 <html lang="ru">
     <head>
         <meta charset="UTF-8">
-        <title>Taks 4</title>
+        <title>Task 4</title>
     </head>
     <body>
         <h1>Введите данные питомца</h1>
