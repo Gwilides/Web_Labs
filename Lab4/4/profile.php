f18739c30cd7ed3d04e42896260d066649f2923c
Gwilides <83014200+Gwilides@users.noreply.github.com>
2025-04-05 23:29:37 +0200

    Updated profile.php

--- Lab4/4/profile.php
+++ Lab4/4/profile.php
@@ -8,3 +8,18 @@ if (!isset($_SESSION['user'])) {
 
 $user = $_SESSION['user'];
 ?>
+
+<!DOCTYPE html>
+<html lang="ru">
+    <head>
+        <meta charset="UTF-8">
+        <title>Профиль питомца</title>
+    </head>
+    <body>
+        <h1>Данные</h1>
+        <p><strong>Имя:</strong> <?php echo htmlspecialchars($user['name']); ?></p>
+        <p><strong>Вид:</strong> <?php echo htmlspecialchars($user['type']); ?></p>
+        <p><strong>Возраст:</strong> <?php echo htmlspecialchars($user['age']); ?></p>
+        <a href="form.php">На Главную</a>
+    </body>
+</html>
