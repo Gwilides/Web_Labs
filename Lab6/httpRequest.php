253e675978ccc14ccbe8e7a7926f77ab3ebc11aa
Gwilides <83014200+Gwilides@users.noreply.github.com>
2025-05-10 16:50:57 +0200

    Created httpRequest.php

---
+++ Lab6/httpRequest.php
@@ -0,0 +1,22 @@
+<?php
+function sendHttpRequest($url, $params = [], $method = 'GET') {
+    $curl = curl_init();
+    
+    if ($method === 'GET' && !empty($params)) {
+        $url .= '?' . http_build_query($params);
+    }
+    
+    if ($method === 'POST') {
+        curl_setopt($curl, CURLOPT_POST, true);
+        curl_setopt($curl, CURLOPT_POSTFIELDS, $params);
+    }
+    
+    curl_setopt($curl, CURLOPT_URL, $url);
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+    
+    $response = curl_exec($curl);
+    curl_close($curl);
+    
+    return $response;
+}
+
